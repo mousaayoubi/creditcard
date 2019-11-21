0dd28f590270e663d7012d2141198bb66dd0acde
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-11-21 17:33:26 +0200

    Update files

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
 const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
 const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
 const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
+const sample = [4, 	5, 	5, 	6, 	7, 	3, 	7, 	5, 	8, 	6, 	8, 	9, 	9, 	8, 	5, 5]
 
 // All invalid credit card numbers
 const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
@@ -26,16 +27,27 @@ let newArr = []
 // Add your functions below:
 function validateCred(arr) {
   arr.pop();
+  console.log(arr)
   for (i=arr.length-1; i>-1; i--) {
     newArr.push(arr[i])
   } 
+  console.log(newArr)
   for (i=0; i<arr.length; i++) {
-    if (newArr[i%2] === 0) {
-      newArr[i] = newArr[i]*2
+    if (i%2 === 0) {
+      newArr[i] = newArr[i] * 2
     }
   }
+  console.log(newArr)
+  for (i=0; i<arr.length;i++) {
+    if (newArr[i]>9) {
+      newArr[i] = newArr[i] - 9
+    }
+  }
+  console.log(newArr)
+  newArrSum = newArr.reduce((a,b) => a+b)
+  checkDigit = 10 - (newArrSum % 10)
 }
 
-validateCred(valid1)
-console.log(valid1)
-console.log(newArr)
+validateCred(sample)
+console.log(newArrSum)
+console.log(checkDigit)
